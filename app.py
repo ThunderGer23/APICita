3ef9d3abc1845c5e0275c57167afc2e51465e6f7
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-11-24 22:29:13 -0600

    Bien por ahora 24/11/2022 10:29:40

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from routes.red import red
+from docs import tags_metadata
+from os import environ as env
+from notigram import ping
+
+ping(env['TOKEN'], 'Servidor API para las citas arriba')
+# Creating a FastAPI object.
+app = FastAPI(
+    title= "API para las citas",
+    description= "API que determina si la cita de un documento esta en formato APA o IEEE",
+    version = "1.0",
+    contact={
+        "name": "Luis Gerardo Baesa Ramirez, Susana Itzel Garduño Sandoval, Arturo Emmanuel Toledo Aguado",
+        "url": "https://github.com/ThunderGer23/APICita",
+        "email": "dp@x-force.example.com",
+    },
+    openapi_tags= tags_metadata
+)
+
+# Importing the `user` module from the `routes` folder.
+app.include_router(red)
