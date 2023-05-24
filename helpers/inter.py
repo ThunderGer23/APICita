80f3eac78366fbc11743b0490ef3d1d1d01b6a7c
ThunderGer <Jaames11@hotmail.com>
2023-05-23 23:59:17 -0600

    O.o

--- helpers/inter.py
+++ helpers/inter.py
@@ -3,6 +3,8 @@ def interpretacion_cita ( ref ):
   for cita in ref:
     if cita >=0.5 :
       interprete.append('CITA APA')
+    elif len(cita)< 25:
+      interprete.append('CITA Erronea')
     else:
       interprete.append('CITA IEEE')
   return interprete
